d5f8913e71b3de2a17c0a74759f6dfe2213f4041
Marcela Rosales <marcelarosalesj@gmail.com>
2019-07-25 11:43:24 -0500

    histogram: add support for images of 1 channel
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- object_detection/histogram.cpp
+++ object_detection/histogram.cpp
@@ -27,9 +27,10 @@ Mat Histogram::generate() {
 
 	/// Compute the histograms:
 	calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-	calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-	calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
-
+	if(bgr_planes.size() == 3) {
+		calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+		calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+	}
 	// Draw the histograms for B, G and R
 	int hist_w = 512; int hist_h = 400;
 	int bin_w = cvRound( (double) hist_w/histSize );
@@ -38,21 +39,24 @@ Mat Histogram::generate() {
 
 	/// Normalize the result to [ 0, histImage.rows ]
 	normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-	normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-	normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
+	if(bgr_planes.size() == 3) {
+		normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+		normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+	}
 	/// Draw for each channel
 	for( int i = 1; i < histSize; i++ )
 	{
 	    line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
 			     Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
 			     Scalar( 255, 0, 0), 2, 8, 0  );
-	    line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-			     Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-			     Scalar( 0, 255, 0), 2, 8, 0  );
-	    line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-			     Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-			     Scalar( 0, 0, 255), 2, 8, 0  );
+	    if(bgr_planes.size() == 3) {
+                    line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+                                     Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+                                     Scalar( 0, 255, 0), 2, 8, 0  );
+                    line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+                                     Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+                                     Scalar( 0, 0, 255), 2, 8, 0  );
+	    }
 	}
 	return histImage;
 }
