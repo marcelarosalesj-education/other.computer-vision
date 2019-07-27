85850b822a9b09ebd0dd53adfd414f66a9efbc17
Marcela Rosales <marcelarosalesj@gmail.com>
2019-07-27 15:49:02 -0500

    basic: fix indentation and parametrize input
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- object_detection/basic.cpp
+++ object_detection/basic.cpp
@@ -1,29 +1,36 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 #include "histogram.h"
+#include "utilities.cpp"
+
 using namespace std;
 using namespace cv;
 
 int main(int argc, char** argv)
 {
-  string img_dir = "../images/";
-  string img_name = "hand.jpg";
-  string img_input = img_dir + img_name; 
-  // Read Image
-  Mat img = imread(img_input);
-  Mat grey_img;
-  cvtColor(img, grey_img, COLOR_BGR2GRAY);
-  // Generate the histogram
-  Histogram hist = Histogram(grey_img);
-  hist.generate();
-  Mat histImage = hist.get();
-  // Save histogram
-  string hist_name = "../images/hist_" + img_name;
-  imwrite(hist_name, histImage);
-  // Save image
-  string img_output("../images/result_hand.jpg");
-  cout << "Saving output image to " << img_output << endl;
-  imwrite(img_output, grey_img);
+    if(argc < 2) {
+        cout << "Missing arguments." << endl;
+        cout << "Usage: ./a.out <image>" << endl;
+        return 1;
+    }
+    // Input image
+    string img_path = argv[1];
+    string img_name = get_file_name(img_path);
+    // Read Image
+    Mat img = imread(img_path);
+    Mat grey_img;
+    cvtColor(img, grey_img, COLOR_BGR2GRAY);
+    // Generate the histogram
+    Histogram hist = Histogram(grey_img);
+    hist.generate();
+    Mat histImage = hist.get();
+    // Save histogram
+    string hist_name = "../results/hist_" + img_name;
+    imwrite(hist_name, histImage);
+    // Save image
+    string img_output = "../results/hist_grey_" + img_name;
+    cout << "Saving output image to " << img_output << endl;
+    imwrite(img_output, grey_img);
 
   return 0;
 }
