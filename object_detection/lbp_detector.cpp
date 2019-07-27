75f637711065752a85dbb96a1c783e8f83308935
Marcela Rosales <marcelarosalesj@gmail.com>
2019-07-27 17:33:26 -0500

    lbp_detector: add a threshold for the image
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- object_detection/lbp_detector.cpp
+++ object_detection/lbp_detector.cpp
@@ -10,6 +10,23 @@
 using namespace std;
 using namespace cv;
 
+Mat threshold_image(Mat input, int thr1, int thr2){
+    Mat result = Mat::zeros(Size(input.cols, input.rows), CV_8UC1);
+    for(int i=0; i < input.rows; i++){
+        for(int j=0; j < input.cols; j++ ){
+            uint8_t value = input.at<uchar>(i,j);
+            if(value > thr1 && value < thr2){
+                result.at<uchar>(i,j) = value;
+                //result[1].at<uchar>(i,j) = 255;
+                //result[2].at<uchar>(i,j) = 255;
+
+            }
+
+        }
+    }
+    return result;
+}
+
 int main(int argc, char** argv) {
     if(argc < 2) {
         cout << "Missing arguments." << endl;
@@ -35,6 +52,14 @@ int main(int argc, char** argv) {
     // Save histogram
     string hist_name = "../results/lbp_hist_" + img_name;
     imwrite(hist_name, histImage);
+    // After visual analysis, threshold are 100 and 170.
+    // Let's generate three images using the thresholds
+    Mat th_img_1 = threshold_image(grey_img, 0, 100);
+    Mat th_img_2 = threshold_image(grey_img, 100, 170);
+    Mat th_img_3 = threshold_image(grey_img, 170, 255);
+    imwrite("../results/lbp_th_1_"+img_name, th_img_1);
+    imwrite("../results/lbp_th_2_"+img_name, th_img_2);
+    imwrite("../results/lbp_th_3_"+img_name, th_img_3);
     return 0;
 }
 
